9d71e897db790f659f1702f1a311ba93f8424e3e
CyberMayck <hotovec.michal@gmail.com>
2015-02-26 17:42:08 +0100

    variant

--- Solution/CQML/CQML/universal_variable.h
+++ Solution/CQML/CQML/universal_variable.h
@@ -1,125 +1,46 @@
 #pragma once
 
-class UniversalVar;
-
-class AnyTypeVal
+struct Variant
 {
-public:
-	long typeId;
+	long typeID;
+	union
+	{
+		int v_int;
+		long v_long;
+		long long v_long_long;
+		float v_float;
+		double v_double;
+		long double v_long_double;
+		void* v_r_void;
+		CQMLObject* v_r_CQMLObject;
+		//#include all_types.h
+	} value;
+	Variant(int);
+	Variant(long);
+	Variant(long long);
+	Variant(float);
+	Variant(double);
+	Variant(long double);
+	Variant(void*);
+	Variant(CQMLObject*);
+	//#include all_types_constructor.h
 
-	virtual AnyTypeVal* operationAddR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationSubR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationMulR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationDivR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationModR(const UniversalVar& rhs);
-	
-	virtual AnyTypeVal* operationAddR(int rhs);
-	virtual AnyTypeVal* operationSubR(int rhs);
-	virtual AnyTypeVal* operationMulR(int rhs);
-	virtual AnyTypeVal* operationDivR(int rhs);
-	virtual AnyTypeVal* operationModR(int rhs);
-	
-	virtual AnyTypeVal* operationAddL(int lhs);
-	virtual AnyTypeVal* operationSubL(int lhs);
-	virtual AnyTypeVal* operationMulL(int lhs);
-	virtual AnyTypeVal* operationDivL(int lhs);
-	virtual AnyTypeVal* operationModL(int lhs);
-};
+	const Variant& operator=(Variant & rhs);
+	const Variant& operator+=(Variant & rhs);
+	const Variant& operator-=(Variant & rhs);
+	const Variant& operator*=(Variant & rhs);
+	const Variant& operator/=(Variant & rhs);
+	const Variant& operator%=(Variant & rhs);
+	const Variant operator+(Variant & rhs) const;
+	const Variant operator-(Variant & rhs) const;
+	const Variant operator*(Variant & rhs) const;
+	const Variant operator/(Variant & rhs) const;
+	const Variant operator%(Variant & rhs) const;
 
-
-class AnyTypeVal_Int
-	: public AnyTypeVal
-{
-public:
-	int value;
-
-	virtual AnyTypeVal* operationAddR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationSubR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationMulR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationDivR(const UniversalVar& rhs);
-	virtual AnyTypeVal* operationModR(const UniversalVar& rhs);
-	
-	virtual AnyTypeVal* operationAddR(int rhs);
-	virtual AnyTypeVal* operationSubR(int rhs);
-	virtual AnyTypeVal* operationMulR(int rhs);
-	virtual AnyTypeVal* operationDivR(int rhs);
-	virtual AnyTypeVal* operationModR(int rhs);
-	
-	virtual AnyTypeVal* operationAddL(int lhs);
-	virtual AnyTypeVal* operationSubL(int lhs);
-	virtual AnyTypeVal* operationMulL(int lhs);
-	virtual AnyTypeVal* operationDivL(int lhs);
-	virtual AnyTypeVal* operationModL(int lhs);
-};
-
-/*
-class AnyTypeVal_UnsignedLongLong
-	: public AnyTypeVal
-{
-	unsigned long long value;
-
-	virtual AnyTypeVal operator+(const AnyTypeVal& rhs) const;
-};
-
-class AnyTypeVal_LongLong
-{
-	long long value;
-};
-class AnyTypeVal_UnsignedLong
-{
-	unsigned long value;
-};
-class AnyTypeVal_Long
-{
-	long value;
-};
-class AnyTypeVal_UnsignedInt
-{
-	unsigned int value;
-};
-class AnyTypeVal_Int
-{
-	int value;
-};
-class AnyTypeVal_LongDouble
-{
-	long double value;
-};
-class AnyTypeVal_Double
-{
-	double value;
-};
-class AnyTypeVal_Float
-{
-	float value;
-};*/
-
-class UniversalVar
-{
-public:
-	AnyTypeVal* value;
-
-	UniversalVar(AnyTypeVal* v);
-
-	UniversalVar operator=(const UniversalVar& rhs);
-	UniversalVar operator-() const;
-
-
-	UniversalVar friend operator+(const UniversalVar& lhs, const UniversalVar& rhs);
-	UniversalVar friend operator-(const UniversalVar& lhs, const UniversalVar& rhs);
-	UniversalVar friend operator*(const UniversalVar& lhs, const UniversalVar& rhs);
-	UniversalVar friend operator/(const UniversalVar& lhs, const UniversalVar& rhs);
-	UniversalVar friend operator%(const UniversalVar& lhs, const UniversalVar& rhs);
-
-	UniversalVar friend operator+(const UniversalVar& lhs, int);
-	UniversalVar friend operator-(const UniversalVar& lhs, int);
-	UniversalVar friend operator*(const UniversalVar& lhs, int);
-	UniversalVar friend operator/(const UniversalVar& lhs, int);
-	UniversalVar friend operator%(const UniversalVar& lhs, int);
-
-	UniversalVar friend operator+(int, const UniversalVar& rhs);
-	UniversalVar friend operator-(int, const UniversalVar& rhs);
-	UniversalVar friend operator*(int, const UniversalVar& rhs);
-	UniversalVar friend operator/(int, const UniversalVar& rhs);
-	UniversalVar friend operator%(int, const UniversalVar& rhs);
+	int As_int();
+	long As_long();
+	long long As_long_long();
+	float As_float();
+	double As_double();
+	long double As_long_double();
 };
