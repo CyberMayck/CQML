72476b6f66e0ab89ec467f92db955b0a34b1032d
CyberMayck <hotovec.michal@gmail.com>
2015-02-26 20:34:35 +0100

    generic AS function int AS<int>

--- Solution/CQML/CQML/universal_variable.h
+++ Solution/CQML/CQML/universal_variable.h
@@ -1,5 +1,7 @@
 #pragma once
 
+struct CQMLObject;
+
 struct Variant
 {
 	long typeID;
@@ -15,6 +17,39 @@ struct Variant
 		CQMLObject* v_r_CQMLObject;
 		//#include all_types.h
 	} value;
+	//template<typename T>
+	//T AS()(const T& obj) { this->AS}
+	
+	template<typename T> T AS()
+	{
+		switch(typeID)
+		{
+		case TYPE_INT:
+			return value.v_int;
+			break;
+		case TYPE_LONG:
+			return value.v_long;
+			break;
+		case TYPE_LONG_LONG:
+			return value.v_long_long;
+			break;
+		case TYPE_FLOAT:
+			return value.v_float;
+			break;
+		case TYPE_DOUBLE:
+			return value.v_double;
+			break;
+		case TYPE_LONG_DOUBLE:
+			return value.v_long_double;
+			break;
+
+		default:
+			throw 0;
+			break;
+		}
+		return 0;
+	}
+
 	Variant(int);
 	Variant(long);
 	Variant(long long);
@@ -23,6 +58,7 @@ struct Variant
 	Variant(long double);
 	Variant(void*);
 	Variant(CQMLObject*);
+
 	//#include all_types_constructor.h
 
 	const Variant& operator=(Variant & rhs);
@@ -37,10 +73,10 @@ struct Variant
 	const Variant operator/(Variant & rhs) const;
 	const Variant operator%(Variant & rhs) const;
 
-	int As_int();
-	long As_long();
-	long long As_long_long();
-	float As_float();
-	double As_double();
-	long double As_long_double();
+	//int As_int();
+	//long As_long();
+	//long long As_long_long();
+	//float As_float();
+	//double As_double();
+	//long double As_long_double();
 };
